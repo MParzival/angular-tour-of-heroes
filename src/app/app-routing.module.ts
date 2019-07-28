1e868111620902e0ab0168999d206b2be78cad95
Mick Aubin <mick.aubin@gmail.com>
2019-07-28 18:57:41 +0200

    Finale save of tourOfHeros "Avengers"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+// tslint:disable-next-line:import-spacing
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// tslint:disable-next-line:import-spacing
+import { DashboardComponent }   from './dashboard/dashboard.component';
+// tslint:disable-next-line:import-spacing
+import { HeroesComponent }      from './heroes/heroes.component';
+// tslint:disable-next-line:import-spacing
+import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/models/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'SpiderMan' },
+  { id: 12, name: 'Dr Strange' },
+  { id: 13, name: 'Hulk' },
+  { id: 14, name: 'Veuve noire' },
+  { id: 15, name: 'Captain America' },
+  { id: 16, name: 'Iron Man' },
+  { id: 17, name: 'Thor' },
+  { id: 18, name: 'Captain Marvel' },
+  { id: 19, name: 'Valkyrie' },
+  { id: 20, name: 'Black Panther' }
+];
